b1403a97b55db2c25908a428c779c4a493bb6afb
Arkadiusz Kubiszewski <90008035+Shift-Happens@users.noreply.github.com>
2023-06-08 22:48:08 +0200

    Functions that are not done have todo descriptions

--- libary.py
+++ libary.py
@@ -73,7 +73,7 @@ class Biblioteka:
         # zostanie wywoana tutaj przed odpaleniem menu  i po zakonczeniu programu
         # do menu uyje petli while True ze slownikiem wyboru opcji
         self.text_menu()  # wyswietla menu - mozna latwo dodac nowe opcje bez ingerencji w kod
-        wybor = self.weryfikacja_inputu_liczba("Wybierz opcje: ")
+        wybor = self.weryfikacja_czy_liczba("Wybierz opcje: ")
         while True:
             menu = {
                 1: self.__dodaj_ksiazke,
@@ -139,29 +139,54 @@ Inspiracyny quote instancji wczytania menu: {self.quote_dnia()}
     def __sprawdz_historie_ksiazki(self):
         pass
 
+    # todo: funkcja ktrra ma zapisac wydarzenie do historii, jak i jego status
+    @staticmethod
+    def __wydarzenie_do_historii():
+        pass
+
+        def sukces():
+            pass
+
+        def porazka():
+            pass
+
     # todo: napisać funkcje zakoncz_program
     def __zakoncz_program(self):
         # zapisuje wszystkie pliki i konczy program
         pass
 
     @staticmethod
-    def weryfikacja_inputu_liczba(tekst):
+    def weryfikacja_czy_liczba(tekst):
         while True:
             try:
                 wybor = int(input(tekst))
                 return wybor
             except ValueError:
-                print("Wprowadź liczbę!")
+                print("Wprowadź liczbę! Podana wartosc nie jest liczba :c")
 
-    # todo: dodac weryfikacje inputu
     @staticmethod
-    def weryfikacja_inputu_tekst(tekst):
+    def weryfikacja_czy_polskie_znaki(tekst):
+        polskie_znaki = ["ą", "ć", "ę", "ł", "ń", "ó", "ś", "ź", "ż"]
         while True:
-            try:
-                wybor = input(tekst)
-                return wybor
-            except ValueError:
-                print("Wprowadź tekst!")
+            wyrazenie = input(tekst.lower())
+            for znak in wyrazenie:
+                if znak in polskie_znaki:
+                    print("Nie wprowadzaj polskich znaków!")
+                    break
+                else:
+                    return wyrazenie
+
+    # todo: napisać funkcje która sprawdzi czy wartość jest pusta i zwróci None albo wartość
+    def wartosc_pusta(self, wartosc):
+        pass
+
+    # todo: funkcja która wprowadzi parfametry i dane do plików csv
+    def zapisanie_do_pliku_csv(self):
+        pass
+
+    # todo: funkcja która wczyta dane z plików csv
+    def wczytanie_z_pliku_csv(self):
+        pass
 
 
 # todo napisać klase która doda eventy do historii
@@ -173,7 +198,7 @@ class Wydarzenia:
         self.data_wypozyczenia = None
         self.data_oddania = None
 
-    # funkcja która będzie zwracać stringa żeby można było go łatwo zaimplemetować w różnych miejscach
+    # todo: funkcja która będzie zwracać stringa żeby można było go łatwo zaimplemetować w różnych miejscach
     def __str__(self):
         return f"{self.id}, Numer czytacza: {self.numer_czytelnika}, Czy udana:{self.czy_udana}, " \
                f"Data Wypozyczenia{self.data_wypozyczenia}, Data oddania: {self.data_oddania} "
